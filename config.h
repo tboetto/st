32816f4303b074d0acc0d62bf8e8fff2e101a4eb
Tony Boetto <tony@tonyboetto.net>
2023-02-12 14:34:09 -0500

    add alpha to xresources variables

--- config.h
+++ config.h
@@ -213,6 +213,7 @@ ResourcePref resources[] = {
 		{ "borderpx",     INTEGER, &borderpx },
 		{ "cwscale",      FLOAT,   &cwscale },
 		{ "chscale",      FLOAT,   &chscale },
+		{ "alpha",        FLOAT,   &alpha },
 };
 
 /*
